5e303e9973e066134e61c63071eba5aa224f88bd
Qua Nguyen <Nguyenmqua@gmail.com>
2021-07-26 01:53:56 -0600

    photo gallery

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,15 +1,127 @@
 import React from 'react';
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
+  Card, CardImg,
 } from 'reactstrap';
+import "./Image.css"
 
 const Example = (props) => {
+  const images = [
+    {
+      id: 1,
+      pic: "./images/Gallery/1.jpg",
+    },
+    {
+      id:2,
+      pic: "./images/Gallery/2.jpg",
+    },
+    {
+      id:3,
+      pic: "./images/Gallery/3.jpg",
+    },
+    {
+      id:4,
+      pic: "./images/Gallery/4.jpg",
+    },
+    {
+      id:5,
+      pic: "./images/Gallery/5.jpg",
+    },
+    {
+      id: 6,
+      pic: "./images/Gallery/6.jpg",
+    },
+    {
+      id: 7,
+      pic: "./images/Gallery/7.jpg",
+    },
+    {
+      id:8,
+      pic: "./images/Gallery/8.jpg",
+    },
+    {
+      id:9,
+      pic: "./images/Gallery/9.jpg",
+    },
+    {
+      id: 10,
+      pic: "./images/Gallery/10.jpg",
+    },
+    {
+      id:11,
+      pic: "./images/Gallery/11.jpg",
+    },
+    {
+      id:12,
+      pic: "./images/Gallery/12.jpg",
+    },
+    {
+      id:13,
+      pic: "./images/Gallery/13.jpg",
+    },
+    {
+      id:14,
+      pic: "./images/Gallery/14.jpg",
+    },
+    {
+      id:15,
+      pic: "./images/Gallery/15.jpg",
+    },
+    {
+      id:16,
+      pic: "./images/Gallery/16.jpg",
+    },
+    {
+      id:17,
+      pic: "./images/Gallery/17.jpg",
+    },
+    {
+      id:18,
+      pic: "./images/Gallery/18.jpg",
+    },
+    {
+      id:19,
+      pic: "./images/Gallery/19.jpg",
+    },
+    {
+      id:20,
+      pic: "./images/Gallery/20.jpg",
+    },
+    {
+      id:21,
+      pic: "./images/Gallery/21.jpg",
+    },
+    {
+      id:22,
+      pic: "./images/Gallery/22.jpg",
+    },
+    {
+      id:23,
+      pic: "./images/Gallery/23.jpg",
+    },
+    {
+      id:24,
+      pic: "./images/Gallery/24.jpg",
+    },
+    {
+      id:25,
+      pic: "./images/Gallery/25.jpg",
+    },
+    {
+      id:26,
+      pic: "./images/Gallery/26.jpg",
+    },
+    {
+      id:27,
+      pic: "./images/Gallery/27.jpg",
+    }
+  ];
   return (
-    <div>
-      <Card>
-        <CardImg top width="100%" src={props.pic} alt="Picnic" />
+    <div className="gallery">
+      {images.map((image)=>
+      <Card className="galleryCard" id={image.id}>
+        <CardImg className="cardImg" top src={image.pic} alt="Picnic" />
       </Card>
+      )} 
     </div>
   );
 };
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,98 +1,13 @@
 import React from "react";
 import Image from "../components/Image"
-
+import "./gallery.css"
 const Gallery = () => {
-  const images = [
-    {
-      pic: "./images/Gallery/1.jpg",
-    },
-    {
-      pic: "./images/Gallery/2.jpg",
-    },
-    {
-      pic: "./images/Gallery/3.jpg",
-    },
-    {
-      pic: "./images/Gallery/4.jpg",
-    },
-    {
-      pic: "./images/Gallery/5.jpg",
-    },
-    {
-      pic: "./images/Gallery/6.jpg",
-    },
-    {
-      pic: "./images/Gallery/7.jpg",
-    },
-    {
-      pic: "./images/Gallery/8.jpg",
-    },
-    {
-      pic: "./images/Gallery/9.jpg",
-    },
-    {
-      pic: "./images/Gallery/10.jpg",
-    },
-    {
-      pic: "./images/Gallery/11.jpg",
-    },
-    {
-      pic: "./images/Gallery/12.jpg",
-    },
-    {
-      pic: "./images/Gallery/13.jpg",
-    },
-    {
-      pic: "./images/Gallery/14.jpg",
-    },
-    {
-      pic: "./images/Gallery/15.jpg",
-    },
-    {
-      pic: "./images/Gallery/16.jpg",
-    },
-    {
-      pic: "./images/Gallery/17.jpg",
-    },
-    {
-      pic: "./images/Gallery/18.jpg",
-    },
-    {
-      pic: "./images/Gallery/19.jpg",
-    },
-    {
-      pic: "./images/Gallery/20.jpg",
-    },
-    {
-      pic: "./images/Gallery/21.jpg",
-    },
-    {
-      pic: "./images/Gallery/22.jpg",
-    },
-    {
-      pic: "./images/Gallery/23.jpg",
-    },
-    {
-      pic: "./images/Gallery/24.jpg",
-    },
-    {
-      pic: "./images/Gallery/25.jpg",
-    },
-    {
-      pic: "./images/Gallery/26.jpg",
-    },
-    {
-      pic: "./images/Gallery/27.jpg",
-    },
-    {
-        pic: "./images/Gallery/28.jpg"
-    },
-  ];
+  
 
-  return(<div>
-      {images.map((image)=>{
-          <Image  pic={image.pic}/>
-       })} 
+  return(<div className="mainGallery">
+
+          <Image/>
+
   </div>) ;
 };
 
