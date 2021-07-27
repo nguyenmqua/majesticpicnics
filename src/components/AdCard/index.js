f46cf423ce46001759617fcab9c037d909a46b98
Qua Nguyen <Nguyenmqua@gmail.com>
2021-07-26 22:15:44 -0600

    new jumbo image

--- src/components/AdCard/index.js
+++ src/components/AdCard/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import image1 from "../../assets/img/sample2/1.jpg";
 import { Button } from "reactstrap";
-import "./AdCard.css"
+import "./AdCard.css";
 const AdCard = () => {
   return (
-      <div className="adWrapper">
+    <div className="adWrapper">
       <div className="adCard">
-      <div className="adImage">
+        <div className="adImage">
           <img src={image1} />
-      </div>
+        </div>
         <div className="adTitle">
           <h3> BRING YOUR VISION TO LIFE </h3>
-
           <p>
             From birthdays, baby showers, bachelorette, bridal shower, propsal,
             engagement party, anniversaries and more!
           </p>
-           <a href="/contact"><Button className="button" >
-           Book Your Picnics Today!
-          </Button></a>
+          <a href="/contact">
+            <Button className="button">Book Your Picnics Today!</Button>
+          </a>
         </div>
-      </div></div>
+      </div>
+    </div>
   );
 };
 
